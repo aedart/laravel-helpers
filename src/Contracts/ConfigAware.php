b25007ac8e3ab780954f17580e629a4f3b0c5b15
Alin Eugen Deac <aedart@gmail.com>
2015-09-12 14:27:42 +0200

    Interface, trait and unit test for the Config

---
+++ src/Contracts/ConfigAware.php
@@ -0,0 +1,59 @@
+<?php  namespace Aedart\Facade\Helpers\Contracts;
+
+use Illuminate\Contracts\Config\Repository;
+
+/**
+ * <h1>Config Aware</h1>
+ *
+ * Components are able to specify and obtain a configuration repository
+ *
+ * @see \Illuminate\Contracts\Config\Repository
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Facade\Helpers\Traits
+ */
+interface ConfigAware {
+
+    /**
+     * Set the given config
+     *
+     * @param Repository $repository Instance of the Configuration Repository
+     *
+     * @return void
+     */
+    public function setConfig(Repository $repository);
+
+    /**
+     * Get the given config
+     *
+     * If no config has been set, this method will
+     * set and return a default config, if any such
+     * value is available
+     *
+     * @see getDefaultConfig()
+     *
+     * @return Repository|null config or null if none config has been set
+     */
+    public function getConfig();
+
+    /**
+     * Get a default config value, if any is available
+     *
+     * @return Repository|null A default config value or Null if no default value is available
+     */
+    public function getDefaultConfig();
+
+    /**
+     * Check if config has been set
+     *
+     * @return bool True if config has been set, false if not
+     */
+    public function hasConfig();
+
+    /**
+     * Check if a default config is available or not
+     *
+     * @return bool True of a default config is available, false if not
+     */
+    public function hasDefaultConfig();
+}
---
+++ src/Traits/ConfigTrait.php
@@ -0,0 +1,84 @@
+<?php namespace Aedart\Facade\Helpers\Traits;
+
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Support\Facades\Config;
+
+/**
+ * <h1>Config Trait</h1>
+ *
+ * @see \Aedart\Facade\Helpers\Contracts\ConfigAware
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ * @package Aedart\Facade\Helpers\Traits
+ */
+trait ConfigTrait {
+
+    /**
+     * Instance of the Configuration Repository
+     *
+     * @var Repository|null
+     */
+    protected $config = null;
+
+    /**
+     * Set the given config
+     *
+     * @param Repository $repository Instance of the Configuration Repository
+     *
+     * @return void
+     */
+    public function setConfig(Repository $repository) {
+        $this->config = $repository;
+    }
+
+    /**
+     * Get the given config
+     *
+     * If no config has been set, this method will
+     * set and return a default config, if any such
+     * value is available
+     *
+     * @see getDefaultConfig()
+     *
+     * @return Repository|null config or null if none config has been set
+     */
+    public function getConfig() {
+        if (!$this->hasConfig() && $this->hasDefaultConfig()) {
+            $this->setConfig($this->getDefaultConfig());
+        }
+        return $this->config;
+    }
+
+    /**
+     * Get a default config value, if any is available
+     *
+     * @return Repository|null A default config value or Null if no default value is available
+     */
+    public function getDefaultConfig() {
+        return Config::getFacadeRoot();
+    }
+
+    /**
+     * Check if config has been set
+     *
+     * @return bool True if config has been set, false if not
+     */
+    public function hasConfig() {
+        if (!is_null($this->config)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if a default config is available or not
+     *
+     * @return bool True of a default config is available, false if not
+     */
+    public function hasDefaultConfig() {
+        if (!is_null($this->getDefaultConfig())) {
+            return true;
+        }
+        return false;
+    }
+}
