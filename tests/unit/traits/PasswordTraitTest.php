ac3ffd4f70bf8558cec0acdae5ae8f62a4ae85e6
Alin Eugen Deac <aedart@gmail.com>
2015-09-19 15:50:46 +0200

    Password completed

--- tests/unit/traits/PasswordTraitTest.php
+++ tests/unit/traits/PasswordTraitTest.php
@@ -2,6 +2,8 @@
 
 use Aedart\Laravel\Helpers\Traits\PasswordTrait;
 use Illuminate\Contracts\Auth\PasswordBroker;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Str;
 
 /**
  * Class PasswordTraitTest
@@ -10,7 +12,7 @@ use Illuminate\Contracts\Auth\PasswordBroker;
  *
  * @author Alin Eugen Deac <aedart@gmail.com>
  */
-class PasswordTraitTest extends TraitTestCase{
+class PasswordTraitTest extends TraitTestCase {
 
     /**
      * Get a mock for the given trait in question
@@ -45,6 +47,11 @@ class PasswordTraitTest extends TraitTestCase{
      */
     public function canObtainPasswordBroker()
     {
+        // Before we obtain it - we need to make a small configuration
+        // because the test-fixtures in Orchestra doesn't contain it.
+        // We are generating it here, more or less just like Laravel
+        Config::set('app.key', Str::random(32));
+
         $mock = $this->getTraitMock();
 
         $config = $mock->getPassword();
