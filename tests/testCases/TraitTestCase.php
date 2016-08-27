f71e5eef0145e0f33954183b1bdb093ba0f3065c
Alin Eugen Deac <aedart@gmail.com>
2016-08-27 18:06:42 +0200

    Change test-case
    Added a trait testing method, which is specially designed for testing this
    package's traits, while laravel is active / running.

--- tests/testCases/TraitTestCase.php
+++ tests/testCases/TraitTestCase.php
@@ -1,7 +1,9 @@
 <?php
 
+use Aedart\Testing\GST\GetterSetterTraitTester;
 use Aedart\Testing\Laravel\TestCases\unit\UnitWithLaravelTestCase;
 use Illuminate\Support\Facades\Facade;
+use \Mockery as m;
 
 /**
  * Class Trait-Test Case
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\Facade;
  * @package unit\testCases
  */
 abstract class TraitTestCase extends UnitWithLaravelTestCase{
+
+    use GetterSetterTraitTester;
 
     /***********************************************************
      * Helpers and utilities
@@ -29,14 +33,61 @@ abstract class TraitTestCase extends UnitWithLaravelTestCase{
         Facade::setFacadeApplication(null);
     }
 
+    /**
+     * Returns a mock
+     *
+     * @param string $classPath
+     *
+     * @return m\MockInterface
+     */
+    public function mock($classPath)
+    {
+        return m::mock($classPath);
+    }
+
+    /***********************************************************
+     * Custom assertions
+     **********************************************************/
+
+    /**
+     * Assert trait
+     *
+     * @param string $traitClassPath
+     * @param string $expectedDefaultClass
+     * @param string $customDefaultClass
+     */
+    public function assertTrait($traitClassPath, $expectedDefaultClass, $customDefaultClass)
+    {
+        $this->output(sprintf('Asserting "%s"', $traitClassPath));
+
+        $this->guessPropertyNameFor($traitClassPath);
+
+        $traitMock = $this->makeTraitMock($traitClassPath);
+
+        $setterName =  $this->setPropertyMethodName();
+        $getterName = $this->getPropertyMethodName();
+
+        $this->assertInstanceOf($expectedDefaultClass, $traitMock->$getterName(), sprintf('Incorrect default in %s (get default method)', $traitClassPath));
+
+        // Ensures that a value can be set and retrieved
+        $this->assertCanSpecifyAndObtainValue($traitMock, $setterName, $getterName, $this->mock($expectedDefaultClass));
+
+        // Ensure that a custom defined default value is returned by default,
+        // if no other value has been set prior to invoking the `get-property`
+        // method.
+        $this->assertReturnsCustomDefaultValue($traitClassPath, $this->getDefaultPropertyMethodName(), $this->getPropertyMethodName(), $this->mock($customDefaultClass));
+    }
+
     /***********************************************************
      * Abstract methods
      **********************************************************/
 
     /**
+     * @deprecated
+     *
      * Get a mock for the given trait in question
      *
      * @return PHPUnit_Framework_MockObject_MockObject|object
      */
-    abstract public function getTraitMock();
+    public function getTraitMock(){}
 }
