a730b8ab7d5d07be96236c5953aaf5f8ada78a16
Alin Eugen Deac <aedart@gmail.com>
2017-01-27 13:52:24 +0100

    Add Performance TestCase (incomplete)

---
+++ tests/testCases/PerformanceTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+use Aedart\Testing\Laravel\TestCases\unit\UnitWithLaravelTestCase;
+
+/**
+ * Performance TestCase
+ *
+ * @author Alin Eugen Deac <aedart@gmail.com>
+ */
+abstract class PerformanceTestCase extends UnitWithLaravelTestCase
+{
+
+}
